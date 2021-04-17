f00fe254c13671792fbb6bcb27a25be6ef8113e2
Usama Khan <usamasarfraz20151@gmail.com>
2021-04-17 06:08:19 +0500

    Cognito SignIn Changes
    Widget for Cognito Signin inside the project along with other CPP classes were made to ensure that cognito login occure smoothly

---
+++ Source/GameLiftTut/Public/MainMenuWidget.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuWidget.generated.h"
+class UWebBrowser;
+/**
+ * 
+ */
+UCLASS()
+class GAMELIFTTUT_API UMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UMainMenuWidget(const FObjectInitializer& ObjectInitializer);
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY()
+		FString LoginUrl;
+
+	UPROPERTY()
+		UWebBrowser* WebBrowser;
+};
